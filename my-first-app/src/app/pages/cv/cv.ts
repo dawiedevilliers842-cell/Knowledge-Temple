30e1b82d7a35dd83e05e772c0cc2504a70afa1ff
Dawie de Villiers <dment842@gmail.com>
2026-04-27 07:54:22 +0200

    feat: added the cv component and started with some content

--- my-first-app/src/app/pages/cv/cv.ts
+++ my-first-app/src/app/pages/cv/cv.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-cv',
-  imports: [],
+  providers: [provideNativeDateAdapter()],
+
+  imports: [
+    MatButtonModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+  ],
   templateUrl: './cv.html',
   styleUrl: './cv.scss',
 })
-export class Cv {}
+export class Cv { }
